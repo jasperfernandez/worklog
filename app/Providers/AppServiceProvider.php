6a8369ea851285e495c18d178b2e9708d6ec3e5c
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-08-27 14:59:27 +0800

    chore: enhance AppServiceProvider with configuration methods for commands, models, dates, password validation, Vite, and application scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +26,65 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+        $this->configurePasswordValidation();
+        $this->configureVite();
+        $this->configureScheme();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(!app()->isProduction());
+        Model::automaticallyEagerLoadRelationships();
+        Model::unguard();
+    }
+
+    /**
+     * Configure the password validation rules.
+     */
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn() => app()->isProduction() ? Password::min(8)->uncompromised() : null);
+    }
+
+    /**
+     * Configure the dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure vite
+     */
+    public function configureVite(): void
+    {
+        Vite::prefetch(concurrency: 3);
+    }
+
+    /**
+     * Configure application scheme
+     */
+    public function configureScheme(): void
+    {
+        if (app()->isProduction()) {
+            URL::forceHttps();
+        }
     }
 }
