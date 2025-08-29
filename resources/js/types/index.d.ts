cb2fe75865ce5eb39f74e00a5e3fe5464584a6e4
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-08-29 09:35:09 +0800

    chore: add Paginated, Resource, and ResourceCollection types for improved data handling

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -11,6 +11,38 @@ export type AppPageProps<T extends Record<string, unknown> = Record<string, unkn
     ziggy: Config & { location: string };
 };
 
+export type Paginated<T> = {
+    data: T[];
+    links: {
+        first: string;
+        last: string;
+        prev: string | null;
+        next: string | null;
+    };
+    meta: {
+        current_page: number;
+        from: number;
+        last_page: number;
+        path: string;
+        per_page: number;
+        to: number;
+        total: number;
+        links: {
+            url: null | string;
+            label: string;
+            active: boolean;
+        }[];
+    };
+};
+
+export type Resource<T> = {
+    data: T;
+};
+
+export type ResourceCollection<T> = {
+    data: T[];
+};
+
 export interface User {
     id: number;
     name: string;
